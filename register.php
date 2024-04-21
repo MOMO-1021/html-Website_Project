f22ff0a7e83148e52f105e246447353a393cca0f
MOMO-1021 <92034273+MOMO-1021@users.noreply.github.com>
2024-04-21 21:45:29 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Now you can store the registered information in your database
+
+echo "Registration successful!";
+?>
